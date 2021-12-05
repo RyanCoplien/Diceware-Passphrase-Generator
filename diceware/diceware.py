0efe13a43e5b5446e51da55b7daefce089246eb9
Ryan Coplien <44984663+RyanCoplien@users.noreply.github.com>
2021-12-05 03:20:56 -0600

    Reformat and ensure working order

--- diceware/diceware.py
+++ diceware/diceware.py
@@ -6,12 +6,13 @@
 # TODO: Use pip freeze and requirements file for github venv
 # TODO: Maybe make this into a GUI application?
 import secrets
+
 # Word list structure [list number, list file name, dice rolls per word]
-word_lists = (     # TODO: Consider making this an object?
-                [1, "EFF Large List", 5],
-                [2, "EFF Short List", 4],
-                [3, "Original Diceware List", 5]
-             )
+word_lists = (  # TODO: Consider making this an object?
+    [1, "EFF Large List", 5],
+    [2, "EFF Short List", 4],
+    [3, "Original Diceware List", 5]
+)
 
 # Invalid defaults to set variables
 word_count = -1
@@ -29,18 +30,32 @@ def main():
     choose_actions()
 
 
+def prompt_repeat():
+    selection = input("Would you like to generate another passphrase? (Y or N)\n")
+    if selection == "Y":  # TODO: Set this to check for correct string (lowercase and uppercase)
+        create_passphrase()
+    else:
+        exit()
+
+
 def choose_actions():
-    # TODO: Create Config settings to select different word lists from selection
-    selection = input("Choose from the following actions:\n"
-                      "1) Generate Diceware Passphrase\n"
-                      "2) Configure Word Count\n"
-                      "3) Select Word List\n")
+    selection = int(input("Choose from the following actions:\n"
+                          "1) Generate Diceware Passphrase\n"
+                          "2) Configure Word Count\n"
+                          "3) Select Word List\n"
+                          "4) Exit\n"))
     match selection:
-        case 1: create_passphrase()
-        case 2: config_word_count()
-        case 3: config_word_list()
-        case _: choose_actions()
-    # TODO: Utilize configuration file settings in order to continue application
+        case 1:
+            create_passphrase()
+        case 2:
+            config_word_count()
+        case 3:
+            config_word_list()
+        case 4:
+            exit()
+        case _:
+            choose_actions()
+    choose_actions()
 
 
 # Function to initialize configuration settings
@@ -52,27 +67,25 @@ def init():
     if word_count == -1:
         word_count = 5
     if word_list == -1:
-        word_list = 1
+        word_list = 0
     # TODO: Pull from config file here then if no config file, create it using defaults
 
 
 def create_passphrase():
-    print(f"Rolling for {word_count} words using the {word_list}")
-    number_list = generate_numbers(word_count)
+    print(f"Rolling for {word_count} words using {word_lists[word_list][1]}")
+    number_list = generate_numbers()
     print(number_list)
     # parse_words(number_list)
+    prompt_repeat()
 
 
-# TODO: Change function name, split functions if needed
 def config_word_count():
     try:
         # Personal recommendation is no more than 7 words due to reaching limits of human memory,
         # Personal recommendation is 5 or more words due to brute forcing times
         selection = int(input("How many words would you like to do use? You can select 1 - 8 words.\n"))
         if selection > 0 or selection <= 8:
-            print(f"Set word count to {selection}.")
             update_config("Count", selection)
-            # TODO: Set config word count here
         else:
             print("Please between 1 and 8 words!")
             config_word_count()
@@ -88,8 +101,6 @@ def config_word_list():
                           "2) EFF Short List\n"
                           "3) Original Diceware List\n")
         if selection in [1, 2, 3]:
-            # TODO: Send
-            print(f"Set word list to {selection}")
             update_config("List", selection)
         else:
             print("Please select a number 1 - 3")
@@ -108,7 +119,7 @@ def update_config(config_setting, config_option):
         print(f"Set word count to {config_option}")
     elif config_setting == "List":
         word_list = config_option
-        # TODO: Update config file word count settings here
+        # TODO: Update config file word list settings here
         print(f"Set word list to {config_option}")
     else:
         print("There was an error updating config settings - Invalid config setting")
@@ -144,7 +155,7 @@ def dice_roll():
     return secrets.choice("123456")  # Use string, as conversion to int is not needed later
 
 
-def parse_words(number_list):
+def parse_words():
     with open(f"..\\lists\\{word_lists[word_list][1]}.txt") as f:
         line = f.readline()
         while line:
@@ -154,5 +165,5 @@ def parse_words(number_list):
 
 
 if __name__ == "__main__":
-    #main()
-    debug()
+    main()
+    # debug()
